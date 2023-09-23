d81ead179390ad68f5a6258a464f18a951d305f8
Nicolas Alexandrino Ferro <141738096+NicolasAFerro@users.noreply.github.com>
2023-09-22 22:22:20 -0300

    Add files via upload

---
+++ atividade5/financeiro/financeiro/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace financeiro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblSalBruto = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblINSS = new System.Windows.Forms.Label();
+            this.lblIRPF = new System.Windows.Forms.Label();
+            this.lblSalFamilia = new System.Windows.Forms.Label();
+            this.lblSalLiq = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(121, 69);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(160, 20);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome do Funcionário";
+            // 
+            // lblSalBruto
+            // 
+            this.lblSalBruto.AutoSize = true;
+            this.lblSalBruto.Location = new System.Drawing.Point(121, 115);
+            this.lblSalBruto.Name = "lblSalBruto";
+            this.lblSalBruto.Size = new System.Drawing.Size(167, 20);
+            this.lblSalBruto.TabIndex = 1;
+            this.lblSalBruto.Text = "Salário do Funcionário";
+            this.lblSalBruto.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numero de Filho";
+            // 
+            // lblINSS
+            // 
+            this.lblINSS.AutoSize = true;
+            this.lblINSS.Location = new System.Drawing.Point(121, 243);
+            this.lblINSS.Name = "lblINSS";
+            this.lblINSS.Size = new System.Drawing.Size(113, 20);
+            this.lblINSS.TabIndex = 3;
+            this.lblINSS.Text = " Alíquota INSS";
+            this.lblINSS.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // lblIRPF
+            // 
+            this.lblIRPF.AutoSize = true;
+            this.lblIRPF.Location = new System.Drawing.Point(121, 273);
+            this.lblIRPF.Name = "lblIRPF";
+            this.lblIRPF.Size = new System.Drawing.Size(108, 20);
+            this.lblIRPF.TabIndex = 4;
+            this.lblIRPF.Text = "Alíquota IRPF";
+            // 
+            // lblSalFamilia
+            // 
+            this.lblSalFamilia.AutoSize = true;
+            this.lblSalFamilia.Location = new System.Drawing.Point(121, 335);
+            this.lblSalFamilia.Name = "lblSalFamilia";
+            this.lblSalFamilia.Size = new System.Drawing.Size(112, 20);
+            this.lblSalFamilia.TabIndex = 5;
+            this.lblSalFamilia.Text = "Salário Familia";
+            // 
+            // lblSalLiq
+            // 
+            this.lblSalLiq.AutoSize = true;
+            this.lblSalLiq.Location = new System.Drawing.Point(121, 382);
+            this.lblSalLiq.Name = "lblSalLiq";
+            this.lblSalLiq.Size = new System.Drawing.Size(113, 20);
+            this.lblSalLiq.TabIndex = 6;
+            this.lblSalLiq.Text = "Salário Liquido";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 611);
+            this.Controls.Add(this.lblSalLiq);
+            this.Controls.Add(this.lblSalFamilia);
+            this.Controls.Add(this.lblIRPF);
+            this.Controls.Add(this.lblINSS);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSalBruto);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblSalBruto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblINSS;
+        private System.Windows.Forms.Label lblIRPF;
+        private System.Windows.Forms.Label lblSalFamilia;
+        private System.Windows.Forms.Label lblSalLiq;
+    }
+}
+
